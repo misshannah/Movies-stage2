96590375fd0480561c23608f00485c922a4022b2
hannaholukoye <hannaholukoye@gmail.com>
2018-08-04 14:08:49 +0300

    docs: Changes to Documentation Adding the source code and readme file.

---
+++ app/src/main/java/com/olukoye/hannah/movies_stage2/MainActivity.java
@@ -0,0 +1,41 @@
+package com.olukoye.hannah.movies_stage2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null){
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
